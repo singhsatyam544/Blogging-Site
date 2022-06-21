536696cd43592ac9c9009e55f0b701fd6559a61c
Satyam singh <singhsatyamjob544@gmail.com>
2022-06-21 12:46:03 +0530

    updateOne

--- src/model/authorModel.js
+++ src/model/authorModel.js
@@ -3,15 +3,27 @@ const mongoose = require('mongoose')
 const authorSchema = new mongoose.schema({
     fname:{
         type:String,
-        
+        require: true
     }, 
-    lname: {mandatory}, 
-    title: {mandatory, 
-        enum[Mr, Mrs, Miss]},
-         email: {mandatory, valid email, unique},
-          password: {mandatory}
-
-
-
+    lname: {
+        type:String,
+        require:true
+    }, 
+    title: {
+        type:String,
+        enum:[Mr, Mrs, Miss],
+        require:true
+    },
+    email: {
+        type:String,
+        require:true,
+        unique:true
+    },
+    password: { 
+        type: String,
+         required: true 
+    }    
 
 }, {timestamp:true})
+
+module.exports = mongoose.model('Author', authorSchema)
